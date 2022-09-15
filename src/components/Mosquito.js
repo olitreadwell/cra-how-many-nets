460096d2071fe253c5ce3200a40391de7461254b
Oli Treadwell <oliver.treadwell@gmail.com>
2022-09-15 15:56:49 -0700

    move over files from prior how many nets project

---
+++ src/components/Mosquito.js
@@ -0,0 +1,10 @@
+import { EmojiConvertor } from "emoji-js";
+
+function convertToNets(multi) {
+  const emoji = new EmojiConvertor();
+  return emoji.replace_colons(":mosquito:").repeat(multi);
+}
+
+export default function Mosquito() {
+  return <div>{convertToNets(25)}</div>;
+}
---
+++ src/components/MosquitoForm.js
@@ -0,0 +1,11 @@
+export default function MosquitoForm(props) {
+  return (
+    <form>
+      <label>
+        Price:
+        <input type="text" />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
